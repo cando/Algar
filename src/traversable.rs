e6bd46f4a97e73b74e060c06a361ad52450dca0f
Stefano <stefano.candori@prima.it>
2023-01-23 11:36:39 +0100

    [wip] almost there #2

--- src/traversable.rs
+++ src/traversable.rs
@@ -1,4 +1,4 @@
-use crate::{Applicative, Apply, Foldable, Functor, Monoid};
+use crate::{Applicative, Foldable, Functor, Monoid};
 
 /// `Traversable` represents data structures which can be traversed while perserving the shape.
 /// Helpful to walk through a data structure from left to right,
@@ -14,12 +14,13 @@ pub trait Traversable<'a> {
     fn traverse<F, B: 'a, U, W>(self, f: F) -> W::Wrapped<Vec<B>>
     where
         U: Applicative<'a, Unwrapped = B> + IntoIterator<Item = B>,
-        F: Fn(&Self::Inner) -> U,
+        U::Wrapped<B>: IntoIterator<Item = U::Wrapped<B>>,
+        F: Fn(&Self::Inner) -> U::Wrapped<B>,
         W: Applicative<'a, Unwrapped = Vec<B>, Wrapped<Vec<B>> = W>
             + 'a
             + IntoIterator<Item = Vec<Self::Inner>>,
         <W as Functor<'a>>::Wrapped<Vec<B>>: Applicative<'a> + Monoid,
-        <<W as Functor<'a>>::Wrapped<Vec<B>> as Functor<'a>>::Wrapped<B>: FromIterator<B>;
+        <W as Functor<'a>>::Wrapped<B>: FromIterator<U::Wrapped<B>>;
 }
 
 impl<'a, A: Monoid> Traversable<'a> for Vec<A> {
@@ -27,17 +28,16 @@ impl<'a, A: Monoid> Traversable<'a> for Vec<A> {
     fn traverse<F, B: 'a, U, W>(self, f: F) -> W::Wrapped<Vec<B>>
     where
         U: Applicative<'a, Unwrapped = B> + IntoIterator<Item = B>,
-        F: Fn(&Self::Inner) -> U,
+        U::Wrapped<B>: IntoIterator<Item = U::Wrapped<B>>,
+        F: Fn(&Self::Inner) -> U::Wrapped<B>,
         W: Applicative<'a, Unwrapped = Vec<B>, Wrapped<Vec<B>> = W>
             + 'a
             + IntoIterator<Item = Vec<Self::Inner>>,
         <W as Functor<'a>>::Wrapped<Vec<B>>: Applicative<'a> + Monoid,
-        <<W as Functor<'a>>::Wrapped<Vec<B>> as Functor<'a>>::Wrapped<B>: FromIterator<B>,
+        <W as Functor<'a>>::Wrapped<B>: FromIterator<U::Wrapped<B>>,
     {
         self.foldr(W::of(vec![]), |k, v| {
-            let c = f(v)
-                .into_iter()
-                .collect::<<<W as Functor<'a>>::Wrapped<Vec<B>> as Functor<'a>>::Wrapped<B>>();
+            let c = f(v).into_iter().collect();
 
             k.lift_a2(c, |mut acc, v: B| {
                 acc.push(v);
@@ -54,6 +54,6 @@ mod test {
     fn test_vec_option_traverse() {
         let a = vec![1, 2, 3];
 
-        let result = a.traverse::<_, _, _, Option<Vec<i32>>>(|v| Option::Some(*v));
+        let result = a.traverse::<_, _, Option<i32>, Option<Vec<i32>>>(|v| Option::Some(*v));
     }
 }
